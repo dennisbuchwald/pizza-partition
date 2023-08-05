67ff1998368c16eea8db2119cb781a751a1c8263
Dennis Buchwald <buchwald95@icloud.com>
2023-08-05 18:37:11 +0200

    debug

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,27 +3,24 @@ import styled from "styled-components";
 import PizzaSliceCalculator from "../../components/PizzaSliceCalculator";
 
 const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `;
 
-const StickyImageContainer = styled.div`
-	position: sticky;
-	top: 150px;
-`;
+const StickyImageContainer = styled.div``;
 
 const Index = () => {
-	return (
-		<Container>
-			<StickyImageContainer>
-				<Image src="/logo.png" alt="Company Logo" width={150} height={150} />
-			</StickyImageContainer>
-			<PizzaSliceCalculator />
-		</Container>
-	);
+  return (
+    <Container>
+      <StickyImageContainer>
+        <Image src="/logo.png" alt="Company Logo" width={150} height={150} />
+      </StickyImageContainer>
+      <PizzaSliceCalculator />
+    </Container>
+  );
 };
 
 export default Index;
